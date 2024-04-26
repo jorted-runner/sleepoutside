5bff330d15501e6e179ff0a52f0602e4f6862ad9
jorted-runner <jorted.runner1996@gmail.com>
2024-04-25 20:39:01 -0500

    single quote fixes

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,50 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import * as fs from 'fs';
 
 export default defineConfig({
-  root: "src/",
+  root: 'src/',
 
   build: {
-    outDir: "../dist",
+    outDir: '../dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
+        main: resolve(__dirname, 'src/index.html'),
+        cart: resolve(__dirname, 'src/cart/index.html'),
+        checkout: resolve(__dirname, 'src/checkout/index.html'),
         product1: resolve(
           __dirname,
-          "src/product_pages/cedar-ridge-rimrock-2.html"
+          'src/product_pages/cedar-ridge-rimrock-2.html',
         ),
-        product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
+        product2: resolve(__dirname, 'src/product_pages/marmot-ajax-3.html'),
         product3: resolve(
           __dirname,
-          "src/product_pages/northface-alpine-3.html"
+          'src/product_pages/northface-alpine-3.html',
         ),
         product4: resolve(
           __dirname,
-          "src/product_pages/northface-talus-4.html"
+          'src/product_pages/northface-talus-4.html',
         ),
       },
     },
+    plugins: [],
   },
+  plugins: [
+    {
+      name: 'copy-json-to-dist',
+      writeBundle() {
+        const destDir = resolve(__dirname, 'dist/json');
+
+        // Create the dist/json folder if it doesn't exist
+        if (!fs.existsSync(destDir)) {
+          fs.mkdirSync(destDir, { recursive: true });
+        }
+
+        fs.copyFileSync(
+          resolve(__dirname, 'src/json/tents.json'),
+          resolve(__dirname, 'dist/json/tents.json'),
+        );
+      },
+    },
+  ],
 });

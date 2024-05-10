395f6c979feab5f3f5b2b1b4b0ce1b977dadbdd8
jorted-runner <jorted.runner1996@gmail.com>
2024-05-09 21:19:06 -0500

    dee-individual

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,26 +1,22 @@
 import { renderListWithTemplate, discountPercentage, setSubscript } from './utils.mjs';
 
 function productCardTemplate(product) {
-  if (product.Image.naturalWidth === 0) {
-      return;
-  } else {
-      let discountContent = '';
-      if (product.FinalPrice < product.SuggestedRetailPrice) {
-          const discountPercent = discountPercentage(product.FinalPrice, product.SuggestedRetailPrice);
-          discountContent = `<p class='discount'>${discountPercent.toFixed(0)}% OFF!</p>`;
-      }
-      const content = 
-          `<li class='product-card'>
-              <a href='product_pages/?product=${product.Id}'>
-                  <img src='${product.Image}' alt='Image of ${product.Name}'/>
-                  <h3 class='card__brand'>${product.Brand.Name}</h3>
-                  <h2 class='card__name'>${product.Name}</h2>
-                  <p class='product-card__price'>$${product.FinalPrice}</p>
-                  ${discountContent}
-              </a>
-          </li>`;
-      return content;
-  }
+    let discountContent = '';
+    if (product.FinalPrice < product.SuggestedRetailPrice) {
+        const discountPercent = discountPercentage(product.FinalPrice, product.SuggestedRetailPrice);
+        discountContent = `<p class='discount'>${discountPercent.toFixed(0)}% OFF!</p>`;
+    }
+    const content = 
+        `<li class='product-card'>
+            <a href='product_pages/?product=${product.Id}'>
+                <img src='${product.Image}' alt='Image of ${product.Name}'/>
+                <h3 class='card__brand'>${product.Brand.Name}</h3>
+                <h2 class='card__name'>${product.Name}</h2>
+                <p class='product-card__price'>$${product.FinalPrice}</p>
+                ${discountContent}
+            </a>
+        </li>`;
+    return content;
 }
 
 export default class ProductListing {
@@ -31,7 +27,7 @@ export default class ProductListing {
     }
 
     async init() {
-        const list = await this.dataSource.getData();
+        const list = await this.dataSource.getData(this.category);
         const filteredProducts = list.filter(this.filterList);
         this.renderList(filteredProducts)
     }

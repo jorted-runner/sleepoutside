644cf9015de2e488ab8b486afebf4972c0e7e254
jorted-runner <jorted.runner1996@gmail.com>
2024-05-09 21:33:03 -0500

    Fixed Images

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -9,7 +9,7 @@ function productCardTemplate(product) {
     const content = 
         `<li class='product-card'>
             <a href='product_pages/?product=${product.Id}'>
-                <img src='${product.Image}' alt='Image of ${product.Name}'/>
+                <img src='${product.Images.PrimaryMedium}' alt='Image of ${product.Name}'/>
                 <h3 class='card__brand'>${product.Brand.Name}</h3>
                 <h2 class='card__name'>${product.Name}</h2>
                 <p class='product-card__price'>$${product.FinalPrice}</p>

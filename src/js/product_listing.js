eee6b9ca6ca0006edc7573c1a17cc3d1a2a989b8
Michael Hyer <michaelhy2002@gmail.com>
2024-05-09 20:20:42 -0600

    Fixed a bug

--- src/js/product_listing.js
+++ src/js/product_listing.js
@@ -1,9 +1,9 @@
-import ProductData from './ProductData.js';
-import ProductList from './ProductList.js';
-import { getParam } from './utils.mjs';
+import ProductData from '../js/ProductData.mjs'
+import ProductList from '../js/ProductList.mjs';
+import { getParams } from '../js/utils.mjs';
 
 
-const category = getParam('category');
+const category = getParams('category');
 const dataSource = new ProductData();
 const listElement = document.querySelector('.product-list');
 const myList = new ProductList(category, dataSource, listElement);

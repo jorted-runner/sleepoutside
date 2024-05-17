c6bebeedcbfc69b7eab37c39681fe5a2335ed924
Michael Hyer <michaelhy2002@gmail.com>
2024-05-16 21:46:18 -0600

    Step 4 complete

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -5,7 +5,7 @@ export default class CalculateOrder {
     constructor() {
         this.itemlist = [];
         this.itemTotal = 0;
-        this.shipping = 0;
+        this.shipping = 8;
         this.tax = 0;
         this.orderTotal = 0;
     }
@@ -14,14 +14,14 @@ export default class CalculateOrder {
         this.itemlist = getLocalStorage('so-cart');
         this.calculateItemSummary();
         this.calculateOrdertotal();
+        this.displayOrderTotals();
     }
 
     calculateItemSummary() {
         if (this.itemlist != null) {
-            this.shipping = 8;
             this.itemlist.forEach(item => {
                 this.itemTotal += item.FinalPrice * item.Quantity;
-                this.shipping += 2;
+                this.shipping += 2 * item.Quantity;
             });
         }
     }
@@ -30,22 +30,17 @@ export default class CalculateOrder {
         // calculate the shipping and tax amounts. Then use them to along with the cart total to figure out the order total
         this.tax = this.itemTotal * 0.06;
         this.orderTotal = this.itemTotal + this.tax + this.shipping;
-
-        // display the totals.
-        this.displayOrderTotals();
+        
     }
 
     displayOrderTotals() {
         // once the totals are all calculated display them in the order summary page
-      const orderElement = document.querySelector('#orderTotals');
-      renderWithTemplate(checkoutTemplate, orderElement, this);
-
+      //renderWithTemplate(this.checkoutTemplate, document.querySelector('#orderTotals'));
+       document.querySelector('#orderTotals').innerHTML = `<p>Subtotal: $${this.itemTotal}</p>
+        <p>Shipping: $${this.shipping}</p>
+        <p>Tax: $${this.tax.toFixed(2)}</p>
+        <p>Order Total: $${this.orderTotal.toFixed(2)}</p>`;
     }
 }
 
-function checkoutTemplate(item) {
-  return `<p>Subtotal: ${item.itemTotal}</p>
-   <p>Shipping: ${item.shipping}</p>
-   <p>Tax: ${item.tax}</p>
-   <p>Order Total: ${item.orderTotal}</p>`;
-}
+
--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,4 +1,4 @@
-import ProductData from './ProductData.mjs'
+import ExternalServices from './ExternalServices.mjs'
 import ProductList from './ProductList.mjs';
 import { loadHeaderFooter, getParams } from './utils.mjs';
 
@@ -7,7 +7,7 @@ loadHeaderFooter();
 const category = getParams('category');
 const listElement = document.querySelector('.product-list');
 
-const dataSource = new ProductData();
+const dataSource = new ExternalServices();
 
 if (category != null) {
     const myList = new ProductList(category, dataSource, listElement);
